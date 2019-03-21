043c0d12fcf079c722758d9cb6296c9a3c467b55
AlphaZero <32816599+0AlphaZero0@users.noreply.github.com>
2019-03-21 11:50:04 +0000

    Create Test.py

--- Scripts/Test.py
+++ Scripts/Test.py
@@ -2,4 +2,13 @@
 #-*- coding: utf-8 -*-
 # THOUVENIN Arthur
 ########################
+import codecs # Allows to load a file containing UTF-8 characters
+import numpy as np # Allows to manipulate the necessary table for sklearn
+import os # Allows to modify some things on the os
+import random # Allows to use random variables
+import shutil #allows file copy
+import sys # Allow to modify files on the OS
+import webbrowser # Allow to use url to open a webbrowser
+
+#################################    Main     ###################################################
 print ("hello")
