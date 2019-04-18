0965071cfc1e216a04662b48fb2b4e73fdaacf02
AlphaZero <32816599+0AlphaZero0@users.noreply.github.com>
2019-04-18 11:22:40 +0100

    Update RawML.py

--- MLAnalysis/RawML.py
+++ MLAnalysis/RawML.py
@@ -39,9 +39,27 @@ clfComplementNB=ComplementNB()
 clfGaussianNB=GaussianNB()
 clfMultinomialNB= MultinomialNB()
 vect=TfidfVectorizer()
-
+featuresList=['Section_num','SubType_num','Figure_num','PreCitation','Citation','PostCitation']
 ################################################    Functions     #################################################
 
+def combinations(a):
+    def fn(n,src,got,all):
+        if n==0:
+            if len(got)>0:
+                all.append(got)
+            return
+        j=0
+        while j<len(src):
+            fn(n-1, src[:j], [src[j]] + got, all)
+            j=j+1
+        return
+    all=[]
+    i=0
+    while i<len(a):
+        fn(i,a,[],all)
+        i=i+1
+    all.append(a)
+    return all
 
 ###################################################    Main     ###################################################
 
@@ -54,9 +72,7 @@ clfList=[[clfLR,"Logistic Regression"],
         [clfSVM,"SVM"]]
 
 data=pd.read_csv(filename,header=0,sep="\t")
-#print(data.shape)
-#print(data.head(10))
-#print(data.Categories.value_counts())
+
 data["Categories_num"]=data.Categories.map({"Background":0,
                                             "ClinicalTrials":1,
                                             "Compare":2,
@@ -69,61 +85,48 @@ sectionDict={}
 index=0
 for section in data.Section:
     if section not in sectionDict:
-        sectionDict[section]=bin(index)[3:]
+        sectionDict[section]=index
         index+=1
 data["Section_num"]=data.Section.map(sectionDict)
 subTypeDict={}
 index=0
 for subType in data.SubType:
     if subType not in subTypeDict:
-        subTypeDict[subType]=bin(index)[3:]
+        subTypeDict[subType]=index
         index+=1
 data["SubType_num"]=data.SubType.map(subTypeDict)
 
 ##################################################################
 """
-X=data
-# X_citation=data.Citation
-# X_pre_Citation=data.PreCitation
-# X_post_Citation=data.PostCitation
-y=data.Categories_num
-X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=1)
-
-"""
-#print(data.head(10))
 X=data.Citation
 y=data.Categories_num
-#print(X.shape)
-#print(y.shape)
+
 X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=1)
-# print(X_train.shape)
-# print(y_train.shape)
-# print(X_test.shape)
-# print(y_test.shape)
 
 X_train_dtm=vect.fit_transform(X_train)
 X_test_dtm=vect.transform(X_test)
+"""
+X=data[featuresList]
+y=data.Categories_num
 
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=1)
+X_train_dtm=np.concatenate((X_train[['Section_num']],X_train[['SubType_num']],X_train[['Figure_num']],vect.fit_transform(X_train[['PreCitation']]),vect.fit_transform(X_train[['Citation']]),vect.fit_transform(X_train[['PostCitation']])))
+X_test_dtm=np.concatenate((X_test[['Section_num']],X_test[['SubType_num']],X_test[['Figure_num']],vect.fit_transform(X_test[['PreCitation']]),vect.fit_transform(X_test[['Citation']]),vect.fit_transform(X_test[['PostCitation']])))
 ##################################################################
 
 for clf in clfList:
     start=time.time()
     try:
+        print (X_train_dtm.shape)
+        print (y_train.shape)
         clf[0].fit(X_train_dtm,y_train)
-        # clfSVM.fit(X_train_dtm,y_train)
-        # clfRF.fit(X_train_dtm,y_train)
+        # print (clf.feature_importance)
         y_pred_class=clf[0].predict(X_test_dtm)
     except TypeError:
         clf[0].fit(X_train_dtm.toarray(),y_train)
         y_pred_class=clf[0].predict(X_test_dtm.toarray())
+        # print (clf.feature_importance)
     end=time.time()
-    # y_pred_class_SVM=clfSVM.predict(X_test_dtm)
-    # y_pred_class_RF=clfRF.predict(X_test_dtm)
     target_names=["Background","ClinicalTrials","Compare","Creation","Unclassifiable","Use"]
     print(metrics.classification_report(y_test,y_pred_class,target_names=target_names),metrics.accuracy_score(y_test,y_pred_class),"\t", clf[1],"\t",str(round((end-start),3))+" sec")
     print("#######################################################")
-    # print(metrics.accuracy_score(y_test,y_pred_class_SVM), "SVM")
-    # print(metrics.accuracy_score(y_test,y_pred_class_RF), "Random Forest")
-# print(metrics.confusion_matrix(y_test,y_pred_class))
-# print(X_test[y_pred_class > y_test])# false positive
-#print(X_test[y_pred_class < y_test])# false negative
