c04378238586dbe3e8db224e77148ba558476bd8
AlphaZero <0AlphaZero0@users.noreply.github.com>
2019-05-08 13:01:30 +0100

    update ML & DL

--- MLAnalysis/test.py
+++ MLAnalysis/test.py
@@ -13,7 +13,6 @@ import os
 from sklearn.feature_extraction.text import TfidfVectorizer # Allows transformations of string in number
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 
 from nltk.stem.snowball import SnowballStemmer
@@ -26,9 +25,8 @@ from keras import backend as K
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-
 filename = "Dataset2.csv"
-result_output="ResultDLparam.csv"
+result_output= "ResultDLparam1.csv"# "ResultDLparam.csv"
 average="macro" # binary | micro | macro | weighted | samples
 class_weight = {
 	0 : 15.,
@@ -36,7 +34,6 @@ class_weight = {
 	2 : 15.,
 	3 : 10.}
 epochs = 5
-# input_node = 1280
 activation_input_node = 'relu'
 node1 = 128
 activation_node1 = 'relu'
@@ -142,117 +139,107 @@ data[SubType_num_str] = data.SubType.map(subTypeDict)
 X = data[featuresList]
 y = data.Categories_num
 
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state = 1)
+
 combinations_list = combinations(extra_features)
 
 output_file=codecs.open(result_output,'w',encoding='utf8')
 output_file.write("f1-score\tPrecision\tRecall\tAccuracy\tLoss\tCombination\tToken\tNgram\tLemma\tStem\n")
-for combination in combinations_list:
-	accuracy_list=[]
-	for i in range(0,5):
-		print(str(i)+"/5 runs")
-		X_train,X_test,y_train,y_test = train_test_split(X,y,random_state = 1)
-		vect_X_train,vect_X_test = [],[]
-		vect_tmp=[]
-		for vect in vect_list:
-			if vect[2] in combination:
-				vect_tmp.append(vect[2])
-				print(vect[2])
-				vect_X_train.append(vect[0].fit_transform(X_train[[vect[1]]].fillna('').values.reshape(-1)).todense())
-				vect_X_test.append(vect[0].transform(X_test[[vect[1]]].fillna('').values.reshape(-1)).todense())
+# for combination in combinations_list:
+# 	vect_X_train,vect_X_test = [],[]
+# 	vect_tmp=[]
+# 	for vect in vect_list:
+# 		if vect[2] in combination:
+# 			vect_tmp.append(vect[2])
+# 			print(vect[2])
+# 			vect_X_train.append(vect[0].fit_transform(X_train[[vect[1]]].fillna('').values.reshape(-1)).todense())
+# 			vect_X_test.append(vect[0].transform(X_test[[vect[1]]].fillna('').values.reshape(-1)).todense())
 
-		vect_X_train.extend((
-			X_train[[Section_num_str]].values,
-			X_train[[SubType_num_str]].values,
-			X_train[[Figure_num_str]].values))
-		vect_X_test.extend((
-			X_test[[Section_num_str]].values,
-			X_test[[SubType_num_str]].values,
-			X_test[[Figure_num_str]].values))
-		
-		X_train_dtm = np.concatenate(vect_X_train, axis = 1)
-		X_train_dtm = tf.keras.utils.normalize(X_train_dtm, axis = 1)
+# 	vect_X_train.extend((
+# 		X_train[[Section_num_str]].values,
+# 		X_train[[SubType_num_str]].values,
+# 		X_train[[Figure_num_str]].values))
+# 	vect_X_test.extend((
+# 		X_test[[Section_num_str]].values,
+# 		X_test[[SubType_num_str]].values,
+# 		X_test[[Figure_num_str]].values))
+	
+# 	X_train_dtm = np.concatenate(vect_X_train, axis = 1)
+# 	X_train_dtm = tf.keras.utils.normalize(X_train_dtm, axis = 1)
 
-		X_test_dtm = np.concatenate(vect_X_test, axis = 1)
-		X_test_dtm = tf.keras.utils.normalize(X_test_dtm, axis = 1)
+# 	X_test_dtm = np.concatenate(vect_X_test, axis = 1)
+# 	X_test_dtm = tf.keras.utils.normalize(X_test_dtm, axis = 1)
 
-		print (X_train_dtm[0].shape[1])
-		model = tf.keras.models.Sequential([
-			tf.keras.layers.Dense(1280, activation = activation_input_node, input_dim=X_train_dtm[0].shape[1]),# maybe this line check github
-			tf.keras.layers.Dense(node1, activation = activation_node1),
-			tf.keras.layers.Dense(node2, activation = activation_node2),
-			tf.keras.layers.Dense(output_node, activation = activation_output_node),
-			])
+# 	print (X_train_dtm[0].shape[1])
+# 	model = tf.keras.models.Sequential([
+# 		tf.keras.layers.Dense(X_train_dtm[0].shape[1], activation = activation_input_node, input_dim=X_train_dtm[0].shape[1]),
+# 		tf.keras.layers.Dense(node1, activation = activation_node1),
+# 		tf.keras.layers.Dense(node2, activation = activation_node2),
+# 		tf.keras.layers.Dense(output_node, activation = activation_output_node),
+# 		])
 
-		model.compile(
-			optimizer="adam",
-			loss="sparse_categorical_crossentropy",
-			metrics=['accuracy'])
+# 	model.compile(
+# 		optimizer="adam",
+# 		loss="sparse_categorical_crossentropy",
+# 		metrics=['accuracy'])
 
-		model.fit(
-			X_train_dtm,
-			y_train,
-			epochs = epochs,
-			batch_size = 20,
-			class_weight = class_weight)
+# 	model.fit(
+# 		X_train_dtm,
+# 		y_train,
+# 		epochs = epochs,
+# 		batch_size = 20,
+# 		class_weight = class_weight)
 
-		val_loss, val_acc = model.evaluate(X_test_dtm, y_test)
+# 	val_loss, val_acc = model.evaluate(X_test_dtm, y_test)
 
-		result = model.predict(X_test_dtm)
-		
-		y_pred=[]
-		for sample in result:
-			y_pred.append(np.argmax(sample))
-		
-		f1_score = round(metrics.f1_score(y_test, y_pred, average = average)*100,3)
-		precision = round(metrics.precision_score(y_test, y_pred, average = average)*100,3)
-		recall = round(metrics.recall_score(y_test, y_pred, average = average)*100,3)
-		accuracy_list.append(metrics.accuracy_score(y_test,y_pred))
+# 	result = model.predict(X_test_dtm)
+	
+# 	y_pred=[]
+# 	for sample in result:
+# 		y_pred.append(np.argmax(sample))
+	
+# 	f1_score = round(metrics.f1_score(y_test, y_pred, average = average)*100,3)
+# 	precision = round(metrics.precision_score(y_test, y_pred, average = average)*100,3)
+# 	recall = round(metrics.recall_score(y_test, y_pred, average = average)*100,3)
 
-	accuracy_mean = 0
-	for accuracy in accuracy_list:
-		accuracy_mean = float(accuracy_mean) + float(accuracy)
-	accuracy_mean = accuracy_mean/len(accuracy_list)
-	print(
-		metrics.classification_report(y_test,y_pred,target_names = target_names),
-		"Cross score : "+str(round(accuracy_mean*100,3)),
-		"Accuracy score : "+str(round(metrics.accuracy_score(y_test,y_pred)*100,3)),
-		"\tF1_score : "+str(f1_score),
-		"\tPrecision : "+str(precision),
-		"\tRecall : "+str(recall),
-		"\n#######################################################")
+# 	print(
+# 		metrics.classification_report(y_test,y_pred,target_names = target_names),
+# 		"Accuracy score : "+str(round(metrics.accuracy_score(y_test,y_pred)*100,3)),
+# 		"\tF1_score : "+str(f1_score),
+# 		"\tPrecision : "+str(precision),
+# 		"\tRecall : "+str(recall),
+# 		"\n#######################################################")
 
-	output_file.write(str(f1_score))
-	output_file.write("\t")
-	output_file.write(str(precision))
-	output_file.write("\t")
-	output_file.write(str(recall))
-	output_file.write("\t")
-	output_file.write(str(val_acc*100))
-	output_file.write("\t")
-	output_file.write(str(round(accuracy_mean*100,3)))
-	output_file.write("\t")
-	output_file.write(str(val_loss))
-	output_file.write("\t")
-	output_file.write(str(vect_tmp))
-	output_file.write("\t")
-	if token in vect_tmp:
-		output_file.write("True")
-	else:
-		output_file.write("False")
-	output_file.write("\t")
-	if ngram in vect_tmp:
-		output_file.write("True")
-	else:
-		output_file.write("False")
-	output_file.write("\t")
-	if lemma in vect_tmp:
-		output_file.write("True")
-	else:
-		output_file.write("False")
-	output_file.write("\t")
-	if stem in vect_tmp:
-		output_file.write("True")
-	else:
-		output_file.write("False")
-	output_file.write("\n")
-		
+# 	output_file.write(str(f1_score))
+# 	output_file.write("\t")
+# 	output_file.write(str(precision))
+# 	output_file.write("\t")
+# 	output_file.write(str(recall))
+# 	output_file.write("\t")
+# 	output_file.write(str(val_acc*100))
+# 	output_file.write("\t")
+# 	output_file.write(str(val_loss))
+# 	output_file.write("\t")
+# 	output_file.write(str(vect_tmp))
+# 	output_file.write("\t")
+# 	if token in vect_tmp:
+# 		output_file.write("True")
+# 	else:
+# 		output_file.write("False")
+# 	output_file.write("\t")
+# 	if ngram in vect_tmp:
+# 		output_file.write("True")
+# 	else:
+# 		output_file.write("False")
+# 	output_file.write("\t")
+# 	if lemma in vect_tmp:
+# 		output_file.write("True")
+# 	else:
+# 		output_file.write("False")
+# 	output_file.write("\t")
+# 	if stem in vect_tmp:
+# 		output_file.write("True")
+# 	else:
+# 		output_file.write("False")
+# 	output_file.write("\n")
+	
